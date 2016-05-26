f8bb1b48ebdb97c9e2da29ea9ebc32d90065b2c5
Andrey Vorobyov <orangevoran@gmail.com>
2016-05-26 22:17:19 +0600

    Initial commit

---
+++ i3script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+i3status --config ~/.i3status.conf | while :
+do
+    read line
+    LG=$(setxkbmap -query | awk '/layout/{print $2}')
+    if [ $LG == "us" ]
+    then
+        dat="[{ \"full_text\": \"LANG: $LG\", \"color\":\"#00009E\" },"
+    else
+        dat="[{ \"full_text\": \"LANG: $LG\", \"color\":\"#C60101\" },"
+    fi
+    echo "${line/[/$dat}" || exit 1
+done
+
